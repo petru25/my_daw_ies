36bdabc103f73eb6761728948920e3c844a51f04
“petru” <petrupastela77@gmail.com>
2020-01-15 09:19:58 +0100

    Nous fitxers

---
+++ febrer.php
@@ -0,0 +1,3 @@
+echo 'pel febrer, carnestoltes el primer';
+echo 'Pel febrer flor a l'ametler';
+echo 'Més tard o més matiner carnaval dins el febrer';
---
+++ index.php
@@ -0,0 +1,3 @@
+echo 'Al gener, tanca la porta i encén el braser';
+echo 'Aigua de gener tot l'any va bé';
+echo 'Les gelades de gener fan florir l'ametller';
